e1fc3c1fbab725257b887a1c911427881fc8a6e4
Hinahim <hinahimthani3@gmail.com>
2023-02-11 19:23:46 +0530

    version 1.0

---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+const footer = () => {
+    return (
+        <>
+            <div class="bottom-container">
+                <div class="bottom-sub-container1">
+                    <ul>
+                        <li> Prompt Generator</li>
+                        <li> Dweep Daily</li>
+                        <li> All Contributors</li>
+                        <li> Your data on Dweep.io</li>
+                        <li> Contribute to Dweep</li>
+                    </ul>
+                </div>
+                <div class="bottom-sub-container2">
+                    <ul>
+                        <li> Dweep.io</li>
+                        <li> Made with love in India</li>
+                        <li>
+                            <img src="linkedin.png" alt="linkedin" /> <img src="instagram.png" alt="instagram" />
+                        </li>
+                        <li id="dontshow"> D</li>
+                        <li> hello@dweep.io</li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+const header = () => {
+    return (
+        <><div class="first-container">
+            <div class="first-sub-container">
+                <h2>
+                    An inspiring design delivered to your inbox every morning
+                </h2>
+                <p class="paragraph1">
+                    Our team scouts the internet for the best designs, illustrations and art and delivers a truly inspiring
+                    one
+                    every day to your inbox
+                </p>
+                <h3>
+                    Show me how it looks
+                </h3>
+                <input id="txtbox" type="textbox" placeholder="Your e-mail address" />
+                <button class="btn" type="submit" value="Register Now"> Register Now</button>
+                <img class="man-img" src="img man.jpg" alt="man" />
+                <p class="paragraph2"> Free - No Spam - No Data Sharing</p>
+
+            </div>
+        </div>
+        </>
+    );
+}
+export default header;
